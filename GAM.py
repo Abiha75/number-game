79932dcec8140dfa3eaeb50e87e6e7c4f36d62c5
Abiha75 <abihaausaf75@gmail.com>
2024-07-15 14:06:17 -0700

    Update GAM.py

--- GAM.py
+++ GAM.py
@@ -11,9 +11,7 @@ if btn_choice1:
 
     txt_guess = int(st.text_input('Enter a number between 1 and 4: ', 1))
 
-     if txt_guess:
-        txt_guess = int(txt_guess)
-        if txt_guess == num:
+    if txt_guess == num:
         st.write('You win')
         st.balloons()
     else:
@@ -31,11 +29,9 @@ if btn_choice2:
 
     txt_guess2 = int(st.text_input('Enter a number between 5 and 7: ', 1))
     
-        if txt_guess2:
-            txt_guess2 = int(txt_guess2)
-            if txt_guess2 == num1:
-                st.write('You win')
-                st.balloons()
+        if txt_guess2 == num1:
+            st.write('You win')
+            st.balloons()
         else:
             html_str = f""" <h1 style='text-align: left; color: #FF4433;'> sorry. Try again. </h1> """
             st.markdown(html_str, unsafe_allow_html=True)
