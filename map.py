40cd7014c68cbc276bae066b3857c45ff259e0b5
Abiha75 <abihaausaf75@gmail.com>
2024-07-16 13:54:42 -0700

    Update map.py

--- map.py
+++ map.py
@@ -1,1 +1,67 @@
+import streamlit as st
+import streamlit_agraph import agraph, Node, Edge, Config
+import pandas as pd
+st.set_page_config(layout="wide")
+from streamlit_agraph.config import Config, ConfigBuilder
 
+final_vd = pd.read_cav(r'./final_top51.cav')
+
+with st.selectbox(
+  "Please slect your Type:"
+  ('CVA', 'IHD', 'CM', 'ARR', 'VD', 'CMD'))
+
+final_arr_short = final_vd[final_vd.Condition == option]
+
+nodes = []
+edges = []
+
+df_genes = dict()
+
+st.title('Knowledge graph')
+df_genes=dict(enumerate(final_arr_short.Protein.unique()))
+for i in df_genes:
+            nodes.append( Node(id=df_genes[i],
+                          label=def_genes[i],
+                               size = 25,
+                               shape = "diamond",
+                               color = '#0000BB'
+                              )
+df_disease = pd.DatatFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name", "count"])
+df_disease = df_diseases.sort_index(axis = 0, ascending=True)
+df_disease = df_disease[df_disease.name !='na']
+for indec, row in df_disease.iterrows():
+
+            nodes.append( Node(id = row['name'],
+                          label = row['name'],
+                          size = 200,
+                               shape = "circle",
+                               color = '#00FFFF'
+                              )
+                        )
+
+df_connections = final_arr_short.filter(items=['Protein', 'neighbour_name']).drop_duplicates()
+d_connectios = df_connections[df_connections.neighbour_name !='na']
+for index, row in df_connections.iterrows():
+          edges.append( Edge(source = row['Protein'],
+                        label = "--",
+                        target = row['neighbour_name'],
+                            )
+                      )
+df_mconnections = final_arr_short.filter(items=['Protein', 'Condition']).drop_duplicates()
+for index, row in df_mconnections.iterrows():
+          edges.append( Edge(source=row['Condition'],
+                          label="--",
+                          target=row['Protein'],
+                          )
+                      )
+
+config_builder = ConfigBuilder(nodes)
+config = config_builder.build()
+
+config.save("config.json")
+
+config = Config(from_json="config.json")
+
+return_value = agraph(nodes = nodes,
+                      edges=edges,
+                      config=config)
