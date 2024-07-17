ffa4becfff3de6bf6273335c44bb09b0120ab792
Abiha75 <abihaausaf75@gmail.com>
2024-07-17 10:11:08 -0700

    Update map2.py

--- map2.py
+++ map2.py
@@ -21,6 +21,13 @@ if option == "disease":
     'Please select your Type:',
     ('CVA', 'IHD', 'CM', 'ARR', 'VD', 'CHD'))
 
+else:
+  with st.sidebar:
+    option = st.selectbox(
+    'Please select your Type:',
+    ('heart faliure', 'liver dysfunction', 'cancer', 'liver fibrosis', 'kidney dysfunction'))
+    
+
 final_arr_short = final_vd[final_vd.Condition == option]
 
 nodes = []
@@ -51,70 +58,7 @@ for index, row in df_disease.iterrows():
                               )
                         )
 
-df_condition = dict()
-df_condition=dict(enumerate(final_arr_short.Condition.unique()))
-for k in df_condition:
-            nodes.append( Node(id=df_condition[k],
-                        label=f"   {option}      ",
-                        size=200,
-                        shape="circle",
-                        color='#00FFFF'
-                        )
-                    ) 
 
-df_connections = final_arr_short.filter(items=['Protein', 'neighbour_name']).drop_duplicates()
-df_connections = df_connections[df_connections.neighbour_name !='na']
-for index, row in df_connections.iterrows():
-          edges.append( Edge(source = row['Protein'],
-                        label = "--",
-                        target = row['neighbour_name'],
-                            )
-                      )
-df_mconnections = final_arr_short.filter(items=['Protein', 'Condition']).drop_duplicates()
-for index, row in df_mconnections.iterrows():
-          edges.append( Edge(source=row['Condition'],
-                          label="--",
-                          target=row['Protein'],
-                          )
-                      )
-
-config_builder = ConfigBuilder(nodes)
-config = config_builder.build()
-
-config.save("config.json")
-
-config = Config(from_json="config.json")
-
-return_value = agraph(nodes=nodes,
-             edges=edges,
-             config=config)
-
-
-
-else:
-  (
-  with st.sidebar:
-    option = st.selectbox(
-    'Please select your Type:',
-    ('heart faliure', 'liver dysfunction', 'cancer', 'liver fibrosis', 'kidney dysfunction'))
-
-final_arr_short = final_vd[final_vd.Condition == option]
-
-nodes = []
-edges = []
-
-df_genes = dict()
-
-st.title('Knowledge graph')
-df_genes=dict(enumerate(final_arr_short.Protein.unique()))
-for i in df_genes:
-            nodes.append( Node(id=df_genes[i],
-                          label=df_genes[i],
-                               size = 25,
-                               shape = "diamond",
-                               color = '#0000BB'
-                              )
-                        )
 df_coMorbid = pd.DataFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name", "count"])
 df_coMorbid = df_coMorbid.sort_index(axis = 0, ascending=True)
 df_coMorbid = df_coMorbid[df_coMorbid.name !='na']
@@ -164,4 +108,5 @@ config = Config(from_json="config.json")
 
 return_value = agraph(nodes=nodes,
              edges=edges,
-             config=config))
+             config=config)
+
