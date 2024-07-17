935d4565f5cf49d002ef84a2824acefa3055e6bb
Abiha75 <abihaausaf75@gmail.com>
2024-07-17 10:25:27 -0700

    Update map2.py

--- map2.py
+++ map2.py
@@ -7,22 +7,20 @@ from streamlit_agraph.config import Config, ConfigBuilder
 final_vd = pd.read_csv(r'./final_top51.csv')
 
 with st.sidebar:
-  option = st.selectbox(
+  option_type = st.selectbox(
     'Would you like to look at diseases or comorbidity?',
     ('disease', 'comorbidity'))
 
-  
-if option == "disease":
+if option_type == "disease":
   with st.sidebar:
     option = st.selectbox(
     'Please select your Type:',
     ('CVA', 'IHD', 'CM', 'ARR', 'VD', 'CHD'))
-
 else:
   with st.sidebar:
     option = st.selectbox(
     'Please select your Type:',
-    ('heart faliure', 'liver dysfunction', 'cancer', 'liver fibrosis', 'kidney dysfunction'))
+    ('heart failure', 'liver dysfunction', 'cancer', 'liver fibrosis', 'kidney dysfunction'))
     
 
 final_arr_short = final_vd[final_vd.Condition == option]
@@ -33,16 +31,13 @@ edges = []
 df_genes = dict()
 
 st.title('Knowledge graph')
-df_genes=dict(enumerate(final_arr_short.Protein.unique()))
+df_genes = dict(enumerate(final_arr_short.Protein.unique()))
 for i in df_genes:
-            nodes.append( Node(id=df_genes[i],
-                          label=df_genes[i],
-                               size = 25,
-                               shape = "diamond",
-                               color = '#0000BB'
-                              )
-                        )
-
+    nodes.append(Node(id=df_genes[i],
+                      label=df_genes[i],
+                      size=25,
+                      shape="diamond",
+                      color='#0000BB'))
 
 df_counts = pd.DataFrame(final_arr_short.neighbour_name.value_counts().reset_index().values, columns=["name", "count"])
 df_counts = df_counts.sort_index(axis=0, ascending=True)
@@ -55,35 +50,27 @@ for index, row in df_counts.iterrows():
                       shape="square",
                       color='#bf9b30'))
 
-
 df_condition = dict()
-df_condition=dict(enumerate(final_arr_short.Condition.unique()))
+df_condition = dict(enumerate(final_arr_short.Condition.unique()))
 for k in df_condition:
-            nodes.append( Node(id=df_condition[k],
-                        label=f"   {option}      ",
-                        size=200,
-                        shape="circle",
-                        color='#00FFFF'
-                        )
-                    ) 
-
-
+    nodes.append(Node(id=df_condition[k],
+                      label=f"   {option}      ",
+                      size=200,
+                      shape="circle",
+                      color='#00FFFF'))
 
 df_connections = final_arr_short.filter(items=['Protein', 'neighbour_name']).drop_duplicates()
-df_connections = df_connections[df_connections.neighbour_name !='na']
+df_connections = df_connections[df_connections.neighbour_name != 'na']
 for index, row in df_connections.iterrows():
-          edges.append( Edge(source = row['Protein'],
-                        label = "--",
-                        target = row['neighbour_name'],
-                            )
-                      )
+    edges.append(Edge(source=row['Protein'],
+                      label="--",
+                      target=row['neighbour_name']))
+
 df_mconnections = final_arr_short.filter(items=['Protein', 'Condition']).drop_duplicates()
 for index, row in df_mconnections.iterrows():
-          edges.append( Edge(source=row['Condition'],
-                          label="--",
-                          target=row['Protein'],
-                          )
-                      )
+    edges.append(Edge(source=row['Condition'],
+                      label="--",
+                      target=row['Protein']))
 
 config_builder = ConfigBuilder(nodes)
 config = config_builder.build()
@@ -93,6 +80,5 @@ config.save("config.json")
 config = Config(from_json="config.json")
 
 return_value = agraph(nodes=nodes,
-             edges=edges,
-             config=config)
-
+                      edges=edges,
+                      config=config)
